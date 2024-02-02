573445b4059034e8d45430adbdb52465f376e144
Khoai-1234 <158304435+Khoai-1234@users.noreply.github.com>
2024-02-02 20:36:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
-import cmath
 import numpy
 class pyAPL:
     def add(number1,number2):
         if number2 == '':
+            # Conjugate
             return numpy.conj(number1)
         else:
+            # Add
             return numpy.add(number1,number2)
     def minus(number1,number2):
         if number2 == '':
+            # Negate
             return -number1
         else:
+            # Minus
             return number1 - number2
+    def time (number1,number2):
+        if number2 == '':
+            # Sign
+            return numpy.sign(number1)
+        else:
+            # Time
+            return numpy.multiply(number1,number2)
+    def divide(number1,number2):
+        if number2 == '':
+            # Reciprocal
+            return numpy.reciprocal(number1)
+        else:
+            # Divide
+            return number1 / number2
+    def residue(number1,number2):
+        if number2 == '':
+            # Absolute
+            return numpy.absolute(number1)
+        else:
+            # Residue( Modulo)
+            return numpy.fmod(number1,number2)
+    def power(number1,number2):
+        if number2 == '':
+            # Exponential
+            return numpy.exp(number1)
+        else:
+            # Power
+            return numpy.power(number1,number2)
+    def logarithm(number1,number2):
+        if number2 == '':
+            # Natural Logarithm
+            return numpy.log(numpy.e,number1)
+        else:
+            return numpy.log(number1,number2)
